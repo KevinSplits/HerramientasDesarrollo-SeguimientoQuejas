46bc3f93e872eaf600b81bc9323316c6397a016e
KevinSplits <130183913+KevinSplits@users.noreply.github.com>
2025-05-23 21:09:50 -0500

    ya funciona el db

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export const PORT = process.env.PORT || 4000;
 export const MONGODB_URI =
-  process.env.MONGODB_URI || "mongodb://localhost/mern-tasks";
+  process.env.MONGODB_URI || "mongodb+srv://kevin100556:68T2LtsT.i-8EK-@cluster0.xy8gp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 export const TOKEN_SECRET = process.env.TOKEN_SECRET || "secret";
 
 export const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:5173";

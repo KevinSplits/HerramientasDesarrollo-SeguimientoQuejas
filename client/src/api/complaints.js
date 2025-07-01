2028f2325b0ea65089612dfb1b774b271396b18d
Panlox123 <167274732+Panlox123@users.noreply.github.com>
2025-07-01 14:46:16 -0500

    Funcion Actualizar y Eliminar para registro de quejas

--- client/src/api/complaints.js
+++ client/src/api/complaints.js
@@ -1,6 +1,6 @@
 import axios from "./axios";
 export const getComplaintsRequest = async () => axios.get("/complaints");
 export const createComplaintRequest = async (complaint) => axios.post("/complaints", complaint);
-export const updateComplaintRequest = async (complaint) => axios.put(`/complaints/${complaint._id}`, complaint);
+export const updateComplaintRequest = async (id, complaint) => axios.put(`/complaints/${id}`, complaint, { withCredentials: true });
 export const deleteComplaintRequest = async (id) => axios.delete(`/complaints/${id}`);
 export const getComplaintRequest = async (id) => axios.get(`/complaints/${id}`);

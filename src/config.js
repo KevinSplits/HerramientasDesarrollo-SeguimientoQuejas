18f135e5f469ef388fc8fbdf41b1685a4b13c471
Panlox123 <167274732+Panlox123@users.noreply.github.com>
2025-07-02 13:32:08 -0500

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,6 +1,5 @@
 export const PORT = process.env.PORT || 4000;
-export const MONGODB_URI =
-  process.env.MONGODB_URI || "mongodb+srv://kevin100556:68T2LtsT.i-8EK-@cluster0.xy8gp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+export const MONGODB_URI = "mongodb+srv://kevin100556:68T2LtsT.i-8EK-@cluster0.xy8gp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 export const TOKEN_SECRET = process.env.TOKEN_SECRET || "secret";
 
-export const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:5173";
+export const FRONTEND_URL = "https://herramientasdesarrollo-seguimientoquejas-fec8.onrender.com";
